e54bbce3dcaad1d13f3c283a3223f7d588d8f044
DiogoBabo <diogo.babo@gmail.com>
2021-11-16 14:13:12 +0000

    adsad2

--- aedastuff/cmake-build-release/CMakeFiles/aed2122_p01.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for aed2122_p01.
-# This may be replaced when dependencies are built.
--- aedastuff/cmake-build-release/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Diogo Babo/Desktop/aedastuff/lib/googletest-master
-# Build directory: C:/Users/Diogo Babo/Desktop/aedastuff/cmake-build-release/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
