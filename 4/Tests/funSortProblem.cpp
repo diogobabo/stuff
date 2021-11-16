2a221489fd466e60feb38f66b25a39b03ac25765
DiogoBabo <diogo.babo@gmail.com>
2021-11-16 15:59:27 +0000

    cena 4

--- 4/Tests/funSortProblem.cpp
+++ 4/Tests/funSortProblem.cpp
@@ -7,13 +7,38 @@ FunSortProblem::FunSortProblem() {}
 //-----------------------------------------------------------------
 
 // TODO
+bool sortkey(Product &a,Product &b){
+    if(a.getPrice() == b.getPrice()){
+        return a.getWeight()<b.getWeight();
+    }
+    else{
+        return a.getPrice()<b.getPrice();
+    }
+}
 void FunSortProblem::expressLane(vector<Product> &products, unsigned k) {
-
+        sort(products.begin(),products.end(), sortkey);
+        if(products.size()<k){
+            return;
+        }
+        products.erase(products.begin()+k,products.end());
+        return;
 }
 
 // TODO
 int FunSortProblem::minDifference(const vector<unsigned> &values, unsigned nc) {
-    return 0;
+    if(values.size() < nc){
+        return -1;
+    }
+    if(values.size() == nc){
+        return (*std::max_element(values.begin(), values.end()) - *min_element(values.begin(), values.end()));
+    }
+    vector<unsigned> copy = values;
+    sort(copy.begin(),copy.end());
+    vector<unsigned> v;
+    for(int x = 0; copy.size() > x+nc ;x++){
+        v.push_back(copy.at(x+nc-1) - copy.at(x));
+    }
+    return *min_element(v.begin(),v.end());
 }
 
 
