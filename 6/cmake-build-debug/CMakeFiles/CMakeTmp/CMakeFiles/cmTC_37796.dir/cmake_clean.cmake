92fa778fd08ae91a6867f2b25a51ac13d11f65a3
DiogoBabo <diogo.babo@gmail.com>
2021-11-30 14:15:30 +0000

    cean 6

---
+++ 6/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_37796.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_37796.dir/CMakeCXXCompilerABI.cpp.obj"
+  "cmTC_37796.exe"
+  "cmTC_37796.exe.manifest"
+  "cmTC_37796.pdb"
+  "libcmTC_37796.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_37796.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
