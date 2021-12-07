17f83611f7dea39eeb1d07aa5e8aee7cad5c2ce8
DiogoBabo <diogo.babo@gmail.com>
2021-12-07 14:27:57 +0000

    7

---
+++ 7/Tests/funWithBSTs.cpp
@@ -0,0 +1,46 @@
+// AED 2021/2022 - Aula Pratica 07
+// Pedro Ribeiro (DCC/FCUP) [06/12/2012]
+
+#include "funWithBSTs.h"
+#include "bst.h"
+
+// ----------------------------------------------------------
+// Exercicio 1: Colecao de Cromos
+// ----------------------------------------------------------
+// TODO
+int FunWithBSTs::newBag(const vector<int>& collection, const vector<int>& bag) {
+  return 0;
+}
+
+// ----------------------------------------------------------
+// Exercicio 2: Batalha de Pokemons
+// ----------------------------------------------------------
+// TODO
+int FunWithBSTs::battle(const vector<int>& alice, const vector<int>& bruno) {
+  return 0;
+}
+
+// ----------------------------------------------------------
+// Exercicio 3: Reviews Cinematograficas
+// ----------------------------------------------------------
+
+// ..............................
+// a) Contando Filmes
+// TODO
+int FunWithBSTs::numberMovies(const vector<pair<string, int>>& reviews) {
+  return 0;
+}
+
+// ..............................
+// b) O filme com mais reviews
+// TODO
+void FunWithBSTs::moreReviews(const vector<pair<string, int>>& reviews, int& m, int& n) {
+}
+
+// ..............................
+// c) Os melhores filmes
+// TODO
+vector<string> FunWithBSTs::topMovies(const vector<pair<string, int>>& reviews, double k) {
+    vector<string> answer;
+    return answer;
+}
