2f1462674f24015a57cc1f18e2f6b47e465e35f0
DiogoBabo <diogo.babo@gmail.com>
2021-12-07 16:02:20 +0000

    7/2

--- 7/Tests/funWithBSTs.cpp
+++ 7/Tests/funWithBSTs.cpp
@@ -1,6 +1,8 @@
 // AED 2021/2022 - Aula Pratica 07
 // Pedro Ribeiro (DCC/FCUP) [06/12/2012]
 
+#include <set>
+#include <map>
 #include "funWithBSTs.h"
 #include "bst.h"
 
@@ -9,7 +11,16 @@
 // ----------------------------------------------------------
 // TODO
 int FunWithBSTs::newBag(const vector<int>& collection, const vector<int>& bag) {
-  return 0;
+    std::set<int> temp(collection.begin(),collection.end());
+    std::set<int> temp1(bag.begin(),bag.end());
+    int result = 0;
+    for(auto x : temp1){
+        if(temp.find(x) == temp.end()){
+            result = result+1;
+        }
+    }
+
+    return result;
 }
 
 // ----------------------------------------------------------
@@ -17,7 +28,30 @@ int FunWithBSTs::newBag(const vector<int>& collection, const vector<int>& bag) {
 // ----------------------------------------------------------
 // TODO
 int FunWithBSTs::battle(const vector<int>& alice, const vector<int>& bruno) {
-  return 0;
+    std::multiset<int> aliceSet(alice.begin(),alice.end());
+    std::multiset<int> brunoSet(bruno.begin(),bruno.end());
+    while(not(aliceSet.empty() || brunoSet.empty())){
+        multiset<int>::iterator ali,bru;
+        ali = --aliceSet.end();
+        bru = --brunoSet.end();
+        if(*ali>*bru){
+            int x = *ali - *bru;
+            brunoSet.erase(bru);
+            aliceSet.erase(ali);
+            aliceSet.insert(x);
+        }
+        else if(*ali<*bru){
+            int x = *bru - *ali;
+            brunoSet.erase(bru);
+            aliceSet.erase(ali);
+            brunoSet.insert(x);
+        }else{
+            brunoSet.erase(bru);
+            aliceSet.erase(ali);
+        }
+
+    }
+  return aliceSet.size() - brunoSet.size();
 }
 
 // ----------------------------------------------------------
@@ -28,13 +62,30 @@ int FunWithBSTs::battle(const vector<int>& alice, const vector<int>& bruno) {
 // a) Contando Filmes
 // TODO
 int FunWithBSTs::numberMovies(const vector<pair<string, int>>& reviews) {
-  return 0;
+    std::set<string> temp;
+    for(auto x : reviews){
+        temp.insert(x.first);
+    }
+
+  return temp.size();
 }
 
 // ..............................
 // b) O filme com mais reviews
 // TODO
 void FunWithBSTs::moreReviews(const vector<pair<string, int>>& reviews, int& m, int& n) {
+    map<string,int> temp;
+    for(auto x : reviews){
+        int y = not(temp.end() == temp.find(x.first)) * temp.find(x.first)->second;
+        temp.insert(pair<string,int>(x.first,y));
+    }
+    int a = 0,b = 0;
+    auto it = temp.rbegin();
+    auto range = temp.equal_range(it->first);
+    for(auto x: temp){
+
+    }
+
 }
 
 // ..............................
